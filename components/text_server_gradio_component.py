b00728f99e5c9569fa952b796613d189d5bfc916
anandhperumal3 <97460597+anandhperumal3@users.noreply.github.com>
2022-06-09 14:10:10 -0400

    Add files via upload

--- components/text_server_gradio_component.py
+++ components/text_server_gradio_component.py
@@ -1,13 +1,16 @@
 import warnings
 warnings.simplefilter("ignore")
 import logging
-import json, requests
+from functools import partial
 import torch
-
+from subprocess import Popen
+from lightning.storage import Path
+from lightning.components.python import TracerPythonScript
 from lightning.components.serve import ServeGradio
 from transformers import AutoTokenizer
 from torch.nn import Softmax
-
+import json
+import requests
 import gradio as gr
 
 logger = logging.getLogger(__name__)
@@ -16,20 +19,21 @@ logger = logging.getLogger(__name__)
 class TextServeGradio(ServeGradio):
 
     inputs = gr.inputs.Textbox(lines=2, placeholder="Enter Text Here… ")
-    outputs = gr.outputs.Label(num_top_classes=5)
+    outputs = gr.outputs.Label(num_top_classes=4)
 
     def __init__(self, cloud_compute, *args, **kwargs):
         super().__init__(*args, cloud_compute=cloud_compute, **kwargs)
         self.examples = None
         self.best_model_path = None
-        self._labels = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4'}
+        self.private_ai_synthetic_data = None
+        self._labels = {0: 'negative', 1: 'positive', 2: 'no_impact', 3: 'neutral'}
 
     def run(self, best_model_path):
         self.examples = [ 'with pale blue berries. in these peaceful shades--', 'it flows so long as falls the rain', 'who the man, who, called a brother.']
         self.best_model_path = best_model_path
         super().run()
 
-    def predict(self, text, url=None,mode=None, key=None):
+    def predict(self, text):
         tokenizer = AutoTokenizer.from_pretrained('distilroberta-base')
 
         payload = json.dumps({
@@ -43,7 +47,8 @@ class TextServeGradio(ServeGradio):
         response = requests.request("POST", "http://localhost:8080/deidentify_text", headers=headers, data=payload)
         pseudonymized_text = response.json()["result_fake"]
 
-        tokenized_txt = tokenizer.encode_plus(pseudonymized_text, None, add_special_tokens=True, return_token_type_ids=True)
+        tokenized_txt = tokenizer.encode_plus(pseudonymized_text, None, add_special_tokens=True,
+                                              return_token_type_ids=True)
         tokenized_txt = torch.tensor(tokenized_txt['input_ids']).unsqueeze(0)
         prediction = self.model(tokenized_txt)
         softmax = Softmax(dim=1)
