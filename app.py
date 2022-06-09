5af79fef2f19be356ca0149d9d76ad462fde0ebb
anandhperumal3 <97460597+anandhperumal3@users.noreply.github.com>
2022-06-09 14:07:03 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,35 +3,35 @@ import lightning as L
 from components import PrivateAISyntheticData, PyTorchLightningScript, TextServeGradio
 
 class TrainDeploy(L.LightningFlow):
-    def __init__(self):
+    def __init__(self, host, port):
         super().__init__()
         self.input_path = "./datasets/test2.csv"
         self.output_path = "./datasets/test2_pai_output.csv"
-        self.url = "http://localhost:8080/deidentify_text"
-        self.key = "XXX"
+        self.key = "INTERNAL_TESTING_UNLIMITED_REALLY"
         self.mode = "standard"
         self.private_ai_synthetic_data = PrivateAISyntheticData(key=self.key, mode=self.mode
-                                                                , text_feature="text",
-                                                                url=self.url)
-        self.private_ai_synthetic_data.run(input_text_or_path=self.input_path, action='batch',
-                                           output_path=self.output_path)
+                                                                , text_feature="text", output_path=self.output_path,
+                                                                host= host, port=port)
 
-
-        if self.private_ai_synthetic_data.output_path:
-            self.train_work = PyTorchLightningScript(
-                script_path=ops.join(ops.dirname(__file__), "./train_script.py"),
-                script_args=["--trainer.max_epochs=1", f"--data.data_file={self.private_ai_synthetic_data.output_path}", "--data.text_feature=text"],
-            )
+        self.train_work = PyTorchLightningScript(
+            script_path=ops.join(ops.dirname(__file__), "./train_script.py"),
+            script_args=["--trainer.max_epochs=1", f"--data.data_file={self.private_ai_synthetic_data.output_path}",
+                         "--data.text_feature=text"],
+        )
 
         self.serve_work = TextServeGradio(cloud_compute=L.CloudCompute("cpu", 1))
+        self.serve_work.private_ai_synthetic_data = L.storage.Payload(self.private_ai_synthetic_data.run)
 
     def run(self):
+
+        self.private_ai_synthetic_data.run(input_text_or_path=self.input_path, action='batch')
 
         # 1. Run the python script that trains the model
         self.train_work.run()
 
         # 2. when a checkpoint is available, deploy
         if self.train_work.best_model_path:
+
             self.serve_work.run(self.train_work.best_model_path)
 
     def configure_layout(self):
@@ -39,4 +39,4 @@ class TrainDeploy(L.LightningFlow):
         tab_2 = {"name": "Interactive demo", "content": self.serve_work}
         return [tab_1, tab_2]
 
-app = L.LightningApp(TrainDeploy())
+app = L.LightningApp(TrainDeploy(host="localhost", port="8080"))
